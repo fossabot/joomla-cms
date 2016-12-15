dd2d090dc1941d016327ab2258a6b63775ebab1c
Brian Teeman <brian@teeman.net>
2016-12-15 14:25:27 +0000

    layout and css

--- plugins/fields/lightbox/layouts/field/prepare/lightbox.php
+++ plugins/fields/lightbox/layouts/field/prepare/lightbox.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  Fields.Youtube
+ * @subpackage  Fields.Lightbox
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -22,21 +22,25 @@ if (!$value)
 	return;
 }
 
-// extract the Youtube video id 
-preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $value, $match);
-$youtube_id = $match[1];
-
 // Loading the language
-JFactory::getLanguage()->load('plg_fields_youtube', JPATH_ADMINISTRATOR);
+JFactory::getLanguage()->load('plg_fields_lightbox', JPATH_ADMINISTRATOR);
 
 $doc = JFactory::getDocument();
 
-// Adding the CSS to make the Youtube video responsive
-JHtml::_('stylesheet', 'plg_fields_youtube/youtube_embed.css', array('version' => 'auto', 'relative' => true));
+// Adding the CSS for the lightbox
+JHtml::_('stylesheet', 'plg_fields_lightbox/lightbox.css', array('version' => 'auto', 'relative' => true));
 
 // Youtube Video container
-$buffer  = '<div class="embed-container"><iframe src="//www.youtube.com/embed/';
-$buffer .= $youtube_id;
-$buffer .= '" frameborder="0" allowfullscreen></iframe></div';
+//$buffer  = '<div class="embed-container"><iframe src="//www.youtube.com/embed/';
+//$buffer .= $youtube_id;
+//$buffer .= '" frameborder="0" allowfullscreen></iframe></div';
+//
+//echo $buffer;
+
+$buffer  = '<div class="lightboxdiv"><a href="#img1"><img src="';
+$buffer .= $lightboximage;
+$buffer .= '" class="thumbnail"></a><a href="#_" class="lightbox" id="img1"><img src="';
+$buffer .= $lightboximage;
+$buffer .= '"></a></div>';
 
 echo $buffer;
